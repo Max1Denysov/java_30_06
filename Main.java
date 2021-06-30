90242aa02688e605f6199a01e0230c757f1d3372
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-06-30 21:35:34 +0300

    eight commit

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Box{
     double width,height,depth;
+    Box(){
+        System.out . println (" Koнcтpyиpoвaниe объекта Вох") ; 
+        width = 10;
+        height = 2;
+        depth = 30;
+    }
     double volume(){
         
        return  width * height * depth;
@@ -9,15 +15,9 @@ class Main{
     public static void main(String args[]){
         Box mybox1 = new Box();
         Box mybox2 = new Box();
-        
+       
 
-        mybox1.width = 10;
-        mybox1.height = 20;
-        mybox1.depth = 15;
-
-        mybox2.width = 160;
-        mybox2.height = 620;
-        mybox2.depth = 15;
+       
 
         
         System . out . println ( " Oбъeм равен " + mybox1.volume() ) ;
