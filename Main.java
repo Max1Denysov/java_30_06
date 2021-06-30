5aefef7470de2ffed227e167a685988f6ac8bb10
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-06-30 21:27:00 +0300

    seven commit

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ class Main{
     public static void main(String args[]){
         Box mybox1 = new Box();
         Box mybox2 = new Box();
-        double vol;
+        
 
         mybox1.width = 10;
         mybox1.height = 20;
@@ -19,10 +19,10 @@ class Main{
         mybox2.height = 620;
         mybox2.depth = 15;
 
-        vol = mybox1.volume();
-        System . out . println ( " Oбъeм равен " + vol ) ;
-        vol = mybox2.volume();
-        System . out . println ( " Oбъeм равен " + vol ) ;
+        
+        System . out . println ( " Oбъeм равен " + mybox1.volume() ) ;
+         
+        System . out . println ( " Oбъeм равен " + mybox2.volume() ) ;
 
 
     }
