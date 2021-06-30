ecadbcd02c93ef979f474fd6227983040e2f68ee
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-06-30 20:38:32 +0300

    four commit

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Box{
     double width,height,depth;
+    void volume(){
+        
+        System.out.println("Object = ");
+        System.out.println( width * height * depth);
+    }
 }
 class Main{
     public static void main(String args[]){
@@ -13,10 +18,9 @@ class Main{
         mybox2.width = 160;
         mybox2.height = 620;
         mybox2.depth = 15;
-        vol = mybox1.width * mybox1.height * mybox1.depth;
-        System.out.println( "Oбъeм равен " + vol );
-        vol = mybox2.width * mybox2.height * mybox2.depth;
-        System.out.println( "Oбъeм равен " + vol );
+
+        mybox1.volume();
+        mybox2.volume();
 
 
     }
